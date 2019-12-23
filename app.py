2215d59860a1810981d410df7f58127ac12d9dd9
daodaoshi <58846742+daodaoshi@users.noreply.github.com>
2019-12-23 20:40:46 +0800

    creat edit and delete item by form

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 import click
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 WIN = sys.platform.startswith('win')
@@ -13,6 +13,7 @@ else:
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = prefix + os.path.join(app.root_path, 'data.db')
+app.config["SECRET_KEY"] = 'dev'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # 关闭对模型修改的监控
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
@@ -29,29 +30,75 @@ class Movie(db.Model):  # 表名将会是 movie
     year = db.Column(db.String(4))  # 电影年份
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # 获取表单数据
+        title = request.form.get('title')
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向主页
+        # 保存表单数据到数据库
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
+
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
 
 
-# @app.route('/user/<name>')
-# def user_page(name):
-#     return 'User: %s' % name
-#
-#
-# @app.route('/test')
-# def test_url_for():
-#     # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
-#     print(url_for('hello'))  # 输出：/
-#     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-#     print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
-#     print(url_for('user_page', name='peter'))  # 输出：/user/peter
-#     print(url_for('test_url_for'))  # 输出：/test
-#     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-#     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-#     return 'Test page'
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
 
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+
+    # @app.route('/user/<name>')
+    # def user_page(name):
+    #     return 'User: %s' % name
+    #
+    #
+    # @app.route('/test')
+    # def test_url_for():
+    #     # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
+    #     print(url_for('hello'))  # 输出：/
+    #     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    #     print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    #     print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    #     print(url_for('test_url_for'))  # 输出：/test
+    #     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    #     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    #     return 'Test page'
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])  # 限定只接受 POST 请求
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)  # 获取电影记录
+    db.session.delete(movie)  # 删除对应的记录
+    db.session.commit()  # 提交数据库会话
+    flash('Item deleted.')
+    return redirect(url_for('index'))  # 重定向回主页
 
 @app.errorhandler(404)
 def page_not_found(e):
